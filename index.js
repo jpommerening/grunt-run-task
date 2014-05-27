cf534b3f41bd4a141982e02b1169da15070e45f8
Jonas Pommerening <jonas.pommerening@gmail.com>
2014-05-27 21:19:24 +0200

    Fixes

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 var grunt = require('grunt');
+var log = require('grunt-legacy-log');
+var Writable = require('stream').Writable;
+var inherits = require('util').inherits;
 var multiTasks = [];
 
+function BufferStream(done, options) {
+  Writable.call(this, options);
+
+  var data = [];
+
+  this._write = function (chunk, encoding, callback) {
+    data.push(chunk);
+    callback();
+  };
+  this.on('finish', function () {
+    done(null, Buffer.concat(data));
+  });
+  this.on('error', function (err) {
+    done(err);
+  });
+}
+inherits(BufferStream, Writable);
+
 grunt.registerMultiTask = (function (obj, method) {
-  return function(name) {
+  return function (name) {
     multiTasks.push(name);
     method.apply(obj, arguments);
   };
@@ -13,56 +34,83 @@ function Task(name, config) {
     return new Task(name, config);
   }
 
+  this.grunt = grunt;
+  this.grunt.log = new log.Log({muted: true});
+  this.grunt.verbose = this.grunt.log.verbose;
   this.name = name;
-  this.config = config;
+  this.files = [];
   this.multi = multiTasks.indexOf(name) >= 0;
+  this.stdout = null;
+
+  var c = {};
+  c[this.name] = config || {};
+
+  this.grunt.initConfig(c);
 }
 
-Task.prototype.run = function (/* [arguments...], [done] */) {
+Task.prototype.run = function (/* [arguments...], done */) {
   var args = [].slice.apply(arguments);
-  var task = this.name;
-  var done;
+  var task = this;
+
+  if (task.multi && args.length === 0) {
+    args.push('default');
+  }
+
+  function run(done) {
+    task.target = task.multi ? args[0] : null;
+    task.config = task.grunt.config.get(task.multi ? [task.name, task.target] : task.name);
+    task.files = task.grunt.task.normalizeMultiTaskFiles(task.config);
+
+    var outStream = task.grunt.log.outStream;
+
+    task.grunt.log.options.outStream = new BufferStream(function (err, data) {
+      if (err) {
+        return done(err);
+      }
+      task.stdout = data;
+    });
+
+    task.grunt.task.options({
+      error: function (err) {
+        console.log('error');
+        console.log(task.stdout);
+        task.grunt.log.options.outStream.end();
+        task.grunt.log.options.outStream = outStream;
+        done(err);
+      },
+      done: function () {
+        console.log('done');
+        done()
+      }
+    });
+
+    task.grunt.task.run(task.name + ':' + args.join(':'));
+    task.grunt.task.start({asyncDone: true});
+  }
 
   if (typeof args[args.length-1] === 'function') {
-    done = args.pop();
-  }
-
-  grunt.task.options( {
-    error: function( err ) {
-      done( err );
-    },
-    done: done
-  } );
-  grunt.initConfig( {} );
-
-  if (this.multi) {
-    task += ':default';
-    grunt.config(this.name, {
-      default: this.config
-    });
+    run(args.pop());
+    return this;
   } else {
-    grunt.config(this.name, this.config);
+    return run;
   }
-
-  if (args.length > 0) {
-    task += ':' + args.join(':');
-  }
-
-  grunt.task.run(task);
-  grunt.task.start({ asyncDone: true });
-
-  return this;
 };
 
 Task.prototype.clean = function(/* [files...], [done] */) {
-  var files = [].slice.apply(arguments);
-  var done;
+  var args = [].slice.apply(arguments);
+  var task = this;
 
-  if (typeof files[files.length-1] === 'function') {
-    done = files.pop();
+  function clean(done) {
+    console.log(task.files);
+    done();
   }
 
-  return this;
+  if (typeof args[args.length-1] === 'function') {
+    clean(args.pop());
+    return this;
+  } else {
+    return clean;
+  }
 };
 
 function runTask(name, config /* [arguments...], [done] */) {
@@ -75,6 +123,7 @@ runTask.task = function create(name, config) {
   return new Task(name, config);
 };
 
+runTask.initConfig = grunt.initConfig.bind(grunt);
 runTask.loadTasks = grunt.loadTasks.bind(grunt);
 runTask.loadNpmTasks = grunt.loadNpmTasks.bind(grunt);
 
